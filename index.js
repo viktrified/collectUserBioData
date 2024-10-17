e9256dbe7769cc92e43ca7fc750f0c1de40effde
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2024-10-17 05:43:12 +0100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-prompt("how are you?");
+prompt("What's your name?");
+let club = prompt("What's your favorite football club?");
+prompt("What's your email?");
+let age = prompt("How old are you?");
+
+if (age > 18) {
+  document.getElementById("h1").innerHTML = `Welcome to Cloud9 viewing center!`;
+  document.getElementById(
+    "p1"
+  ).innerHTML = `Thank you using Cloud9. we're rooting for ${club} to win1`;
+} else {
+  alert("wait till you're 18");
+}
